cd6ac2500457be017a1bd15df77bcd447556960d
lzy8339789 <1641189550@qq.com>
2019-01-29 17:54:38 +0800

    springboot整合rabbitmq

---
+++ springboot-rabbitmq/src/main/java/com/github/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.github.springbootrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqApplication.class, args);
+    }
+
+}
+
