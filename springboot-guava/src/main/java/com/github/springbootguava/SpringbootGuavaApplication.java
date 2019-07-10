4b9f448d9b59eaa88c4d230294473411c9efd4bc
lzy8339789 <1641189550@qq.com>
2019-07-11 00:27:58 +0800

    添加springboot-guava

---
+++ springboot-guava/src/main/java/com/github/springbootguava/SpringbootGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.springbootguava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGuavaApplication.class, args);
+    }
+
+}
